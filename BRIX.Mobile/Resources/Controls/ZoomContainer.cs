4ad99fb94b0e67967a650c4d34a2f1a02d661aab
lokizzz <lokizzzzzzzz@gmail.com>
2023-04-20 19:00:32 +0300

    Неплохой вариант кропа;

--- BRIX.Mobile/Resources/Controls/ZoomContainer.cs
+++ BRIX.Mobile/Resources/Controls/ZoomContainer.cs
@@ -1,146 +1,152 @@
-﻿namespace BRIX.Mobile.Resources.Controls
+﻿namespace BRIX.Mobile.Resources.Controls;
+
+public class ZoomContainer : ContentView
 {
-    public class ZoomContainer : ContentView
+    public ZoomContainer()
     {
-        double currentScale = 1;
-        double startScale = 1;
-        double xOffset = 0;
-        double yOffset = 0;
+        PinchGestureRecognizer pinchGesture = new PinchGestureRecognizer();
+        pinchGesture.PinchUpdated += OnPinchUpdated;
+        GestureRecognizers.Add(pinchGesture);
 
-        public ZoomContainer()
+        // Set PanGestureRecognizer.TouchPoints to control the
+        // number of touch points needed to pan
+        PanGestureRecognizer panGesture = new PanGestureRecognizer();
+        panGesture.PanUpdated += OnPanUpdated;
+        GestureRecognizers.Add(panGesture);
+
+        ChildAdded += OnChildAdded;
+    }
+
+    public static readonly BindableProperty ContentXProperty = BindableProperty.Create(
+        nameof(ContentX), typeof(double), typeof(ZoomContainer), 0d
+    );
+
+    public double ContentX
+    {
+        get => (double)GetValue(ContentXProperty);
+        set
         {
-            PinchGestureRecognizer pinchGesture = new PinchGestureRecognizer();
-            pinchGesture.PinchUpdated += OnPinchUpdated;
-            GestureRecognizers.Add(pinchGesture);
-
-            // Set PanGestureRecognizer.TouchPoints to control the
-            // number of touch points needed to pan
-            PanGestureRecognizer panGesture = new PanGestureRecognizer();
-            panGesture.PanUpdated += OnPanUpdated;
-            GestureRecognizers.Add(panGesture);
-        }
-
-
-
-        public static readonly BindableProperty ContentTranslationXProperty = BindableProperty.Create(
-            propertyName: nameof(ContentTranslationX),
-            returnType: typeof(double),
-            declaringType: typeof(ZoomContainer),
-            defaultValue: 0,
-            defaultBindingMode: BindingMode.TwoWay
-        );
-
-        public double ContentTranslationX
-        {
-            get => (double)GetValue(ContentTranslationXProperty);
-            set
+            SetValue(ContentXProperty, value);
+            if (Content != null)
             {
-                SetValue(ContentTranslationXProperty, value);
-                if (Content != null) Content.TranslationX = value;
+                Content.TranslationX = value;
+                OnPropertyChanged(nameof(Content.TranslationX));
             }
         }
+    }
 
-        public static readonly BindableProperty ContentTranslationYProperty = BindableProperty.Create(
-            propertyName: nameof(ContentTranslationY),
-            returnType: typeof(double),
-            declaringType: typeof(ZoomContainer),
-            defaultValue: 0,
-            defaultBindingMode: BindingMode.TwoWay
-        );
+    public static readonly BindableProperty ContentYProperty = BindableProperty.Create(
+        nameof(ContentY), typeof(double), typeof(ZoomContainer), 0d
+    );
 
-        public double ContentTranslationY
+    public double ContentY
+    {
+        get => (double)GetValue(ContentYProperty);
+        set
         {
-            get => (double)GetValue(ContentTranslationYProperty);
-            set
+            SetValue(ContentYProperty, value);
+            if (Content != null)
             {
-                SetValue(ContentTranslationYProperty, value);
-                if (Content != null) Content.TranslationY = value;
+                Content.TranslationY = value;
+                OnPropertyChanged(nameof(Content.TranslationY));
             }
         }
+    }
 
-        public static readonly BindableProperty ContentScaleProperty = BindableProperty.Create(
-            propertyName: nameof(ContentScale),
-            returnType: typeof(double),
-            declaringType: typeof(ZoomContainer),
-            defaultValue: 0,
-            defaultBindingMode: BindingMode.TwoWay
-        );
+    public static readonly BindableProperty ContentScaleProperty = BindableProperty.Create(
+        nameof(ContentScale), typeof(double), typeof(ZoomContainer), 1d
+    );
 
-        public double ContentScale
+    public double ContentScale
+    {
+        get => (double)GetValue(ContentScaleProperty);
+        set
         {
-            get => (double)GetValue(ContentScaleProperty);
-            set
+            SetValue(ContentScaleProperty, value);
+            if (Content != null)
             {
-                SetValue(ContentScaleProperty, value);
-                if (Content!= null) Content.Scale = value;
+                Content.Scale = value;
+                OnPropertyChanged(nameof(Content.Scale));
             }
         }
+    }
 
-        void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+    double currentScale = 1;
+    double startScale = 1;
+    double xOffset = 0;
+    double yOffset = 0;
+
+    void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+    {
+        if (e.Status == GestureStatus.Started)
         {
-            if (e.Status == GestureStatus.Started)
-            {
-                // Store the current scale factor applied to the wrapped user interface element,
-                // and zero the components for the center point of the translate transform.
-                startScale = Content.Scale;
-                Content.AnchorX = 0;
-                Content.AnchorY = 0;
-            }
-            if (e.Status == GestureStatus.Running)
-            {
-                // Calculate the scale factor to be applied.
-                currentScale += (e.Scale - 1) * startScale;
-                currentScale = Math.Max(1, currentScale);
-
-                // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
-                // so get the X pixel coordinate.
-                double renderedX = Content.X + xOffset;
-                double deltaX = renderedX / Width;
-                double deltaWidth = Width / (Content.Width * startScale);
-                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
-
-                // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
-                // so get the Y pixel coordinate.
-                double renderedY = Content.Y + yOffset;
-                double deltaY = renderedY / Height;
-                double deltaHeight = Height / (Content.Height * startScale);
-                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
-
-                // Calculate the transformed element pixel coordinates.
-                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
-                double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
-
-                // Apply translation based on the change in origin.
-                Content.TranslationX = Math.Clamp(targetX, -Content.Width * (currentScale - 1), 0);
-                Content.TranslationY = Math.Clamp(targetY, -Content.Height * (currentScale - 1), 0);
-
-                // Apply scale factor
-                Content.Scale = currentScale;
-            }
-            if (e.Status == GestureStatus.Completed)
-            {
-                // Store the translation delta's of the wrapped user interface element.
-                xOffset = Content.TranslationX;
-                yOffset = Content.TranslationY;
-            }
+            // Store the current scale factor applied to the wrapped user interface element,
+            // and zero the components for the center point of the translate transform.
+            startScale = ContentScale;
         }
-
-        void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        if (e.Status == GestureStatus.Running)
         {
-            switch (e.StatusType)
-            {
-                case GestureStatus.Running:
-                    // Translate and ensure we don't pan beyond the wrapped user interface element bounds.
-                    Content.TranslationX = Math.Max(Math.Min(0, xOffset + e.TotalX), -Math.Abs(Content.Width - DeviceDisplay.MainDisplayInfo.Width));
-                    Content.TranslationY = Math.Max(Math.Min(0, yOffset + e.TotalY), -Math.Abs(Content.Height - DeviceDisplay.MainDisplayInfo.Height));
-                    break;
+            // Calculate the scale factor to be applied.
+            currentScale += (e.Scale - 1) * startScale;
+            currentScale = Math.Max(1, currentScale);
 
-                case GestureStatus.Completed:
-                    // Store the translation applied during the pan
-                    xOffset = Content.TranslationX;
-                    yOffset = Content.TranslationY;
-                    break;
-            }
+            // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
+            // so get the X pixel coordinate.
+            double renderedX = Content.X + xOffset;
+            double deltaX = renderedX / Width;
+            double deltaWidth = Width / (Content.Width * startScale);
+            double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+
+            // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
+            // so get the Y pixel coordinate.
+            double renderedY = Content.Y + yOffset;
+            double deltaY = renderedY / Height;
+            double deltaHeight = Height / (Content.Height * startScale);
+            double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+
+            // Calculate the transformed element pixel coordinates.
+            double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+            double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+
+            // Apply translation based on the change in origin.
+            ContentX = Math.Clamp(targetX, -Content.Width * (currentScale - 1), 0);
+            ContentY = Math.Clamp(targetY, -Content.Height * (currentScale - 1), 0);
+
+            // Apply scale factor
+            ContentScale = currentScale;
         }
+        if (e.Status == GestureStatus.Completed)
+        {
+            // Store the translation delta's of the wrapped user interface element.
+            xOffset = Content.TranslationX;
+            yOffset = Content.TranslationY;
+        }
+    }
+
+    void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+        switch (e.StatusType)
+        {
+            case GestureStatus.Running:
+                // Translate and ensure we don't pan beyond the wrapped user interface element bounds.
+                Content.TranslationX = ContentX + e.TotalX;
+                Content.TranslationY = ContentY + e.TotalY;
+                break;
+
+            case GestureStatus.Completed:
+                // Store the translation applied during the pan
+                ContentX = Content.TranslationX;
+                ContentY = Content.TranslationY;
+                break;
+        }
+    }
+
+    void OnChildAdded(object sender, ElementEventArgs e)
+    {
+        Content.AnchorX = 0;
+        Content.AnchorY = 0;
+        Content.TranslationX = ContentX;
+        Content.TranslationY = ContentY;
+        Content.Scale = ContentScale;
     }
 }
