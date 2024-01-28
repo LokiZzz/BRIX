d6498a50c8d2853d2fa01cd33512c281afb2cf3e
lokizzz <lokizzzzzzzz@gmail.com>
2024-01-28 14:27:34 +0300

    Убран лишний трайкэтч;

--- BRIX.Lexica/LexisFormatter.cs
+++ BRIX.Lexica/LexisFormatter.cs
@@ -25,40 +25,33 @@ namespace BRIX.Lexica
 
         public string Format(string? format, object? model, IFormatProvider? formatProvider)
         {
-            try
+            if (!Equals(formatProvider))
             {
-                if (!Equals(formatProvider))
-                {
-                    return string.Empty;
-                }
-
-                if (string.IsNullOrEmpty(format))
-                {
-                    throw new ArgumentNullException(nameof(format));
-                }
-
-                if (model == null)
-                {
-                    return string.Empty;
-                }
-
-                string[] spliitedFormat = format.Split('.');
-                string formatType = spliitedFormat[0];
-                string formatValue = spliitedFormat[1];
-
-                switch (formatType)
-                {
-                    case PropertyFormatType:
-                        return FormatProperty(formatValue, model);
-                    case ComplexFormatType:
-                        throw new NotImplementedException();
-                    default:
-                        throw new ArgumentException($"Неизвестный формат {formatType}");
-                }
+                return string.Empty;
             }
-            catch (Exception ex)
+
+            if (string.IsNullOrEmpty(format))
             {
-                return null;
+                throw new ArgumentNullException(nameof(format));
+            }
+
+            if (model == null)
+            {
+                return string.Empty;
+            }
+
+            string[] spliitedFormat = format.Split('.');
+            string formatType = spliitedFormat[0];
+            string formatValue = spliitedFormat[1];
+
+            switch (formatType)
+            {
+                case PropertyFormatType:
+                    return FormatProperty(formatValue, model);
+                case ComplexFormatType:
+                    throw new NotImplementedException();
+                default:
+                    throw new ArgumentException($"Неизвестный формат {formatType}");
             }
         }
 
