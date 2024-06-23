92033ae8c28cfdc5048d459580acddd175faf374
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2024-06-23 14:04:15 +0300

    Аспект вампиризма (только библиотека);

---
+++ BRIX.Library/Aspects/VampirismAspect.cs
@@ -0,0 +1,17 @@
+﻿using BRIX.Library.Mathematics;
+
+namespace BRIX.Library.Aspects
+{
+    /// <summary>
+    /// Аспект, позволяющий восстанавливать здоровье 
+    /// </summary>
+    public class VampirismAspect : AspectBase
+    {
+        public int VampirismPercent { get; set; } = 50;
+
+        public override double GetCoefficient()
+        {
+            return VampirismPercent.ToCoeficient();
+        }
+    }
+}
