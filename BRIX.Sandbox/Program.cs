510b1644a750f3ab962d8195c645711992c786df
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2024-07-18 11:24:08 +0300

    Отступы в мониторе стоимости;

--- BRIX.Sandbox/Program.cs
+++ BRIX.Sandbox/Program.cs
@@ -7,14 +7,3 @@ using BRIX.Utility.Extensions;
 using Newtonsoft.Json;
 using System.Net.Http.Json;
 using System.Text.Json.Serialization;
-
-//ActivationConditionsAspect aspect = new();
-//aspect = aspect.Copy();
-
-//string json = JsonConvert.SerializeObject(aspect);
-//aspect = JsonConvert.DeserializeObject<ActivationConditionsAspect>(json);
-
-//json = JsonConvert.SerializeObject(aspect);
-//aspect = JsonConvert.DeserializeObject<ActivationConditionsAspect>(json);
-
-Console.WriteLine();
