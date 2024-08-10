f95979cbdd12b4716d6b5450f3ad92291e81248b
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2024-08-10 18:14:32 +0300

    Задел на немоту и паралич;

---
+++ BRIX.Library/Effects/MutenessEffect.cs
@@ -0,0 +1,27 @@
+﻿using BRIX.Library.Aspects.TargetSelection;
+using BRIX.Library.Aspects;
+using BRIX.Library.Extensions;
+using BRIX.Library.Characters;
+
+namespace BRIX.Library.Effects
+{
+    /// <summary>
+    /// Немота. Не даёт цели использовать способности, для активации которых персонаж должен мочь говорить.
+    /// </summary>
+    public class MutenessEffect : EffectBase
+    {
+        public override bool IsPositive => false;
+
+        public override List<Type> RequiredAspects =>
+        [
+            typeof(TargetSelectionAspect), typeof(ActivationConditionsAspect), typeof(DurationAspect)
+        ];
+
+        /// <summary>
+        /// Текущее кол-во здоровья у цели, необходимое для того, чтобы эффект на ней сработал.
+        /// </summary>
+        public int HealthThreshold { get; set; } = 10;
+
+        public override int BaseExpCost() => CharacterCalculator.HealthToExp(HealthThreshold) / 4;
+    }
+}
---
+++ BRIX.Library/Effects/ParalysisEffect.cs
@@ -0,0 +1,28 @@
+﻿using BRIX.Library.Aspects.TargetSelection;
+using BRIX.Library.Aspects;
+using BRIX.Library.Extensions;
+using BRIX.Library.Characters;
+
+namespace BRIX.Library.Effects
+{
+    /// <summary>
+    /// Паралич. Не даёт цели использовать способности, для активации которых персонаж должен мочь двигаться, также 
+    /// цель теряет возможность перемещаться никак иначе, как способностями у которых нет соответствующего условия активации.
+    /// </summary>
+    public class ParalysisEffect : EffectBase
+    {
+        public override bool IsPositive => false;
+
+        public override List<Type> RequiredAspects =>
+        [
+            typeof(TargetSelectionAspect), typeof(ActivationConditionsAspect), typeof(DurationAspect)
+        ];
+
+        /// <summary>
+        /// Текущее кол-во здоровья у цели, необходимое для того, чтобы эффект на ней сработал.
+        /// </summary>
+        public int HealthThreshold { get; set; } = 10;
+
+        public override int BaseExpCost() => CharacterCalculator.HealthToExp(HealthThreshold) * 3;
+    }
+}
