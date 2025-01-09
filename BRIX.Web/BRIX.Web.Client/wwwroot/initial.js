d5816af8c8108e91a35bdf6bcb81cdc37bb7469e
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2025-01-09 14:49:49 +0300

    Навёл порядок в App.razor;

---
+++ BRIX.Web/BRIX.Web.Client/wwwroot/initial.js
@@ -0,0 +1,9 @@
+﻿window.hideSplash = () => {
+    var btn = document.getElementById('splash');
+    btn.style.display = 'none';
+};
+
+window.blazorCulture = {
+    get: () => localStorage['BlazorCulture'],
+    set: (value) => localStorage['BlazorCulture'] = value
+};
