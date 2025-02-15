f5246ebb245a07362fa1917414d5cdb072089262
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2025-02-15 18:25:13 +0300

    Саморасширяющееся поле с ограничением по символам;

---
+++ BRIX.Web/BRIX.Web.Client/wwwroot/controls.js
@@ -0,0 +1,20 @@
+﻿function autoExpand(textarea) {
+    textarea.style.height = 'auto'; // Сброс высоты
+    textarea.style.height = (textarea.scrollHeight) + 'px'; // Установка новой высоты
+}
+
+// Экспортируем функцию для использования в Blazor
+window.initializeAutoExpand = function (textareaId) {
+    const textarea = document.getElementById(textareaId); // Находим элемент по ID
+    if (textarea) {
+        textarea.addEventListener('input', () => autoExpand(textarea));
+        autoExpand(textarea); // Инициализация при загрузке
+    }
+};
+
+window.updateTextarea = function (id, value) {
+    const textarea = document.getElementById(id);
+    if (textarea) {
+        textarea.value = value;
+    }
+}
