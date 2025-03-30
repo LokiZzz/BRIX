35fd0e2cc9335b926046ed77a413b15beb491822
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2025-03-30 14:37:29 +0300

    Типизация и биндинги для компонента OptionsTab;

---
+++ BRIX.Web/BRIX.Web.Client/Components/Shared/OptionsTabEmpty.cs
@@ -0,0 +1,14 @@
+﻿namespace BRIX.Web.Client.Components.Shared
+{
+    /// <summary>
+    /// Пустой вариант компонента для случаев, когда переход по закладкам не должен присваивать значение какому-либо 
+    /// свойству. Создан для чистоты в Blazor-коде, чтобы не указывать TValue="object" и Value="new object()".
+    /// </summary>
+    public class OptionsTabEmpty : OptionsTab<object>;
+
+    /// <summary>
+    /// Закладка для OptionsTabEmpty, для которой не обязательно указывать свойство Value для определения типа 
+    /// обобщения.
+    /// </summary>
+    public class OptionsTabEmptyItem : OptionsTabItem<object>;
+}
