40af33c139922a0a1f13401be6714cc79a4a73e3
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2025-04-01 20:21:30 +0300

    Небольшая оптимизация на странице аспекта выбора цели и фикс селектора OptionsTab;

--- BRIX.Web/BRIX.Web.Client/Components/Shared/OptionsTabEmpty.cs
+++ BRIX.Web/BRIX.Web.Client/Components/Shared/OptionsTabEmpty.cs
@@ -1,10 +1,23 @@
-﻿namespace BRIX.Web.Client.Components.Shared
+﻿
+
+
+using Newtonsoft.Json.Linq;
+
+namespace BRIX.Web.Client.Components.Shared
 {
     /// <summary>
     /// Пустой вариант компонента для случаев, когда переход по закладкам не должен присваивать значение какому-либо 
     /// свойству. Создан для чистоты в Blazor-коде, чтобы не указывать TValue="object" и Value="new object()".
     /// </summary>
-    public class OptionsTabEmpty : OptionsTab<object>;
+    public class OptionsTabEmpty : OptionsTab<object>
+    {
+        public override void AddItem(OptionsTabItem<object> item)
+        {
+            Items.Add(item);
+            ActiveItem ??= item;
+            StateHasChanged();
+        }
+    }
 
     /// <summary>
     /// Закладка для OptionsTabEmpty, для которой не обязательно указывать свойство Value для определения типа 
