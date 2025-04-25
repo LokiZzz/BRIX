3dee6664a658a5a621a4889cccf49a04d4aec9f1
lokizzzzzzzz <lokizzzzzzzz@gmail.com>
2025-04-25 10:52:28 +0300

    Из лейаута вынесена статистика способности, которая касается конкретно персонажа;

---
+++ BRIX.Web/BRIX.Web.Client/Models/Abilities/EAbilityOwner.cs
@@ -0,0 +1,12 @@
+﻿namespace BRIX.Web.Client.Models.Abilities
+{
+    /// <summary>
+    /// Тип владельца способности.
+    /// </summary>
+    public enum EAbilityOwner
+    {
+        Character = 0,
+        NPC = 1,
+        Artifact = 2
+    }
+}
